e938988b138e3fe457406f0419adff1a0008c586
AdrianNextMD <adriano99pavaleanu@gmail.com>
2021-08-19 18:47:32 +0300

    Initial Commit

---
+++ tekwill-csharp-homeworkApp/tekwill-csharp-homework/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace tekwill_csharp_homework
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
